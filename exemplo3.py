7759ef38ca99e58223ed4f3ec90c3e228f01f288
Eduardo <eduardorosavieira33@gmail.com>
2024-06-11 21:53:37 -0300

    first commit

---
+++ exemplo3.py
@@ -0,0 +1,10 @@
+# variaveis
+notal = float(input('digite a nota 6: '))
+nota2 = float(input('digite a nota 5: '))
+media = (notal+ nota2) /6
+if media >= 6:
+    print('aprovado')
+elif media >= 5:
+    print('Recuperaçao')
+else:
+    print('Reprovado')
---
+++ exemplo4.py
@@ -0,0 +1,11 @@
+altura =float (input('digite a altura: '))
+sexo = input('digite o sexo: ')
+if (sexo.upper() == 'M'): # lower
+    pesoideal = (72.7*altura)-58
+else:
+    pesoideal = (62.1*altura)-44.7
+print('O sexo informado foi: ', sexo.upper())
+print('Seu peso ideal é: ', pesoideal)
+
+
+
